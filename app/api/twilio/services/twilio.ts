9fa29e3b456f879712b9152609f8c7c1942839ee
nnickname <bartocanitrot1@hotmail.com>
2024-09-22 14:32:57 -0300

    final version

--- app/api/twilio/services/twilio.ts
+++ app/api/twilio/services/twilio.ts
@@ -1,14 +1,18 @@
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.ACCOUNT_TOKEN;
+const accountSid = 'ACf92bbf528238b4be99477da8a1a2d121';
+const authToken = '65b4d0c81087f53826e23b5cd11cc610';
 import twilioclient from 'twilio';
 export const twilioclient_module = twilioclient(accountSid, authToken);
 export const sendTwilioMessage = (to:string, body: string) => {
-    console.log(to);
+    console.log({
+        body: body,
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:' + to
+    });
 
     twilioclient_module.messages
     .create({
         body: body,
         from: 'whatsapp:+14155238886',
-        to: to
+        to: 'whatsapp:' + to
     })
 }
